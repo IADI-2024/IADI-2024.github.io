a5ef766472ab7b26d90dd78d2d15dd2560ef9248
João Costa Seco <jrcs@fct.unl.pt>
2024-10-16 11:27:08 +0100

    Lecture 5 slides

--- labs/lab3/SocialApp/src/main/kotlin/org/example/socialapp/presentation/MainController.kt
+++ labs/lab3/SocialApp/src/main/kotlin/org/example/socialapp/presentation/MainController.kt
@@ -1,21 +1,15 @@
 package org.example.socialapp.presentation
 
+import org.example.socialapp.application.MainApp
 import org.example.socialapp.service.OrderService
 import org.example.socialapp.service.ProductService
 import org.example.socialapp.service.UserService
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class MainApp(
-    val userClient: UserService,
-    val orderService: OrderService,
-    val productService: ProductService
-) : MainAPI {
+class MainController(val app:MainApp) : MainAPI {
 
-    override fun hello() =
-        listOf(userClient.hello(),
-               orderService.hello(),
-               productService.hello())
+    override fun hello() = app.hello()
 }
 
 
